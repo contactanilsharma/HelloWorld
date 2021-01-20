3738ad6d2d7e38fd7a023fbe8c480c736628da26
contactanilsharma <anilk.sharma@hotmail.com>
2021-01-20 18:58:32 +0530

    commit

--- Scripts/functions.R
+++ Scripts/functions.R
@@ -1,27 +1,68 @@
-fun_add <- function(x, y){
-  x+y
+fun_add <- function(x, y) {
+  x + y
 }
 
-fun_above10 <- function(x){
+fun_above10 <- function(x) {
   use <- x > 10
   x[use]
 }
 
-fun_above <- function(x,n=4){
+fun_above <- function(x, n = 4) {
   use <- x > n
   x[use]
 }
 
-fun_calcMean <- function(x, remove_na=TRUE){
-     nc <- ncol(x)
-     means <- numeric(nc)
-     for (i in 1:nc){
-       means[i] <- mean(x[,i], na.rm=remove_na)
-     }
-     means
+fun_calcMean <- function(x, remove_na = TRUE) {
+  nc <- ncol(x)
+  means <- numeric(nc)
+  for (i in 1:nc) {
+    means[i] <- mean(x[, i], na.rm = remove_na)
+  }
+  means
 }
 
 
-fun_test <- function(x, y){
-  x^2
+fun_test <- function(x, y) {
+  x ^ 2
 }
+
+y <- 10
+
+f <- function(x) {
+  y <- 2
+  y ^ 2 + g(x)
+}
+
+g <- function(x) {
+  x * y
+}
+
+f(3)
+
+cube <- function(x, n) {
+  x^3
+}
+cube(3)
+
+x <- 1:10
+if(x > 5) {
+  x <- 0
+}
+
+x <- 5
+y <- if(x < 3) {
+  NA
+} else {
+  10
+}
+
+f <- function(x) {
+  g <- function(y) {
+    y + z
+  }
+  z <- 4
+  x + g(x)
+}
+
+z <- 10
+f(3)
